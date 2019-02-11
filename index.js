724b5c563ad3b97eb5ac6da283d9036e65f045ca
Christine Cua <cloudycua@gmail.com>
2019-02-11 17:53:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function lowerCaseDrivers(list) {
   return list.map(function(driver) {
     return driver.toLowerCase();
@@ -17,4 +18,10 @@ function attributesToPhrase(list){
   return list.map(function(driver) {
     return `${driver.name} is from ${driver.hometown}`;
   });
+=======
+function lowerCaseDrivers(drivers) {
+  map(drivers, function(driver)) {
+    driver.toLowerCase();
+  }
+>>>>>>> c39900538bf05542950e00b8c7b87851836194ee
 }
